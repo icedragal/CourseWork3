93e0b47bb52dda11a34ea5aa0b6f0020928dcf41
Icedragal <icedragal@gmail.com>
2023-01-24 12:11:58 +0300

    Initial commit

---
+++ src/test/java/me/egorzhuravlev/coursework3/CourseWork3ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.egorzhuravlev.coursework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
